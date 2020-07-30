073c1842b71683436c70b0baf624c48bb81860e9
Racquel Winkler Smith <racquelsimone@Racquels-MacBook-Air.local>
2020-07-29 20:56:14 -0700

    Added Meal Form (minus price)

--- HomeCooked/forms.py
+++ HomeCooked/forms.py
@@ -1,1 +1,13 @@
 # This is our forms section
+
+
+
+
+class MealForm(forms.ModelForm):
+    class Meta:
+      model = Meal
+      fields = [
+        'name',
+        'description',
+        'image_url',] 
+# price is a different model. How do I link Can you add two models to one form
