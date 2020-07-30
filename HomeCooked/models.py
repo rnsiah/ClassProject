8669cb0f09b7a86b2040e82af37cd9f6976a7fc7
Racquel Winkler Smith <racquelsimone@Racquels-MacBook-Air.local>
2020-07-29 20:28:50 -0700

    added model for meal price per link Kofi sent me earlier

--- HomeCooked/models.py
+++ HomeCooked/models.py
@@ -18,9 +18,19 @@ class Vendor(models.Model):
 
 # Create your models here.
 
-class Meal(models.Model)
-name = models.CharField(max_length=100, blank=False, null=False)
-date_added = models.DateField(auto_now_add=True)
-description = models.CharField(max_length=75, blank=False, null=False)
-review = models.CharField(max_length=100, blank=True, null=True)
-image_url = models.TextField(null= True, blank=True)
+    class Meal(models.Model):
+        name = models.CharField(max_length=100, blank=False, null=False)
+        date_added = models.DateField(auto_now_add=True)
+        description = models.CharField(max_length=75, blank=False, null=False)
+        review = models.CharField(max_length=100, blank=True, null=True)
+        image_url = models.TextField(null= True, blank=True)
+
+class Mealprice(models.Model):
+    item= models.CharField(max_length=200)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.item) +":$" + str(self.price)
+    
+
+    
